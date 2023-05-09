964dc18807ca7a69f66dd86278d5a2e2f34c1ae4
tingyanshen <tingyan.shen@gmail.com>
2023-05-10 02:27:16 +0800

    添加 Cross/Zone @Doodle 组件 Spring 装配依赖

--- doodle-idle-autoconfigure/src/main/java/org/doodle/idle/autoconfigure/cross/server/CrossServerAutoConfiguration.java
+++ doodle-idle-autoconfigure/src/main/java/org/doodle/idle/autoconfigure/cross/server/CrossServerAutoConfiguration.java
@@ -15,12 +15,24 @@
  */
 package org.doodle.idle.autoconfigure.cross.server;
 
+import org.doodle.admin.autoconfigure.client.AdminClientAutoConfiguration;
+import org.doodle.broker.autoconfigure.client.BrokerClientAutoConfiguration;
+import org.doodle.config.autoconfigure.client.ConfigClientAutoConfiguration;
+import org.doodle.console.autoconfigure.client.ConsoleClientAutoConfiguration;
+import org.doodle.excel.autoconfigure.client.ExcelClientAutoConfiguration;
 import org.doodle.idle.cross.server.CrossServerProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@AutoConfiguration
+@AutoConfiguration(
+    after = {
+      BrokerClientAutoConfiguration.class,
+      ConfigClientAutoConfiguration.class,
+      ConsoleClientAutoConfiguration.class,
+      AdminClientAutoConfiguration.class,
+      ExcelClientAutoConfiguration.class
+    })
 @ConditionalOnClass(CrossServerProperties.class)
 @EnableConfigurationProperties(CrossServerProperties.class)
 public class CrossServerAutoConfiguration {}
--- doodle-idle-autoconfigure/src/main/java/org/doodle/idle/autoconfigure/zone/server/ZoneServerAutoConfiguration.java
+++ doodle-idle-autoconfigure/src/main/java/org/doodle/idle/autoconfigure/zone/server/ZoneServerAutoConfiguration.java
@@ -15,12 +15,24 @@
  */
 package org.doodle.idle.autoconfigure.zone.server;
 
+import org.doodle.admin.autoconfigure.client.AdminClientAutoConfiguration;
+import org.doodle.broker.autoconfigure.client.BrokerClientAutoConfiguration;
+import org.doodle.config.autoconfigure.client.ConfigClientAutoConfiguration;
+import org.doodle.console.autoconfigure.client.ConsoleClientAutoConfiguration;
+import org.doodle.excel.autoconfigure.client.ExcelClientAutoConfiguration;
 import org.doodle.idle.zone.server.ZoneServerProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@AutoConfiguration
+@AutoConfiguration(
+    after = {
+      BrokerClientAutoConfiguration.class,
+      ConfigClientAutoConfiguration.class,
+      ConsoleClientAutoConfiguration.class,
+      AdminClientAutoConfiguration.class,
+      ExcelClientAutoConfiguration.class
+    })
 @ConditionalOnClass(ZoneServerProperties.class)
 @EnableConfigurationProperties(ZoneServerProperties.class)
 public class ZoneServerAutoConfiguration {}
