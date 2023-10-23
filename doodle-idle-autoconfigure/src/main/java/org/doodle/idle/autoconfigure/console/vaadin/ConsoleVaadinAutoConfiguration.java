9d7fe8d75525756bfe676ffa713cc0776d0dfe86
石头 <tingyan.shen@gmail.com>
2023-10-23 19:18:00 +0800

    添加放置游戏服/跨服运维管理 console 模块

---
+++ doodle-idle-autoconfigure/src/main/java/org/doodle/idle/autoconfigure/console/vaadin/ConsoleVaadinAutoConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.idle.autoconfigure.console.vaadin;
+
+import org.doodle.boot.vaadin.EnableVaadin;
+import org.doodle.idle.console.vaadin.ConsoleVaadinProperties;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@AutoConfiguration
+@ConditionalOnClass(ConsoleVaadinProperties.class)
+@EnableConfigurationProperties(ConsoleVaadinProperties.class)
+@EnableVaadin(ConsoleVaadinProperties.PREFIX_VIEWS)
+public class ConsoleVaadinAutoConfiguration {}
